1e023a6338823ba4e907d3e1536928ccc2982b8e
unknown <k.jones45@yahoo.com>
2023-04-06 22:15:47 -0400

    Worked on html to have name and status displayed on browser. installed bootstrap css. ran into an error- had to shut down server and restart it. added some comments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+{/*imrs for the import React useState */}
+import React, { useState } from 'react';
+
 
 function App() {
-  return <div>
-    <h3> Hello World </h3>
-  </div>
+{/*status = actual name of state variable.*/}
+{/* setStatus = function responsible for updating statuses */}
+{/*whatever you pass into useState function call that's going to be the intial value of our stateful variable
+which we know it will have a collection of data. like an empty array [] (we'll use empty string for now)*/}
 
+const [status, setStatus] = useState()
+  
+
+return (
+    <div>
+      <table>
+        <thead>
+          <tr> Name </tr>
+        </thead>
+      </table>
+    <h3> status </h3>
+    </div>
   );
+
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App.js';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ ReactDOM.render(<App />,document.getElementById('root'));
 
 
+{/*render function takes in two arguements, the higher componendt-App component and we can use it like
+defining what our root component is. take it compilet and combine it and inject it to the index.html file
+that has 'root'*/}
